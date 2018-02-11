1693a7662ef4818aedd6e6127ec4f4b11b473e86
kingrayhan <rayhan095@gmail.com>
2018-02-11 16:40:50 +0600

    add click play event

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Play drum by keyboard press
 window.addEventListener('keydown' , playDrum);
 function playDrum(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -7,11 +8,25 @@ function playDrum(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     key.classList.add('playing');
 };
+
+// Remove .playing class after transition action over
 const keys = document.querySelectorAll('.key');
 keys.forEach( key => key.addEventListener('transitionend' , removePlayingClass ) );
 function removePlayingClass(e)
 {
     if( e.propertyName != 'transform' ) return;
     this.classList.remove('playing');
-    // console.log(this);
+}
+
+
+// Play music by mouse click
+keys.forEach( key => key.addEventListener('click' , playByMouse ) );
+function playByMouse(e)
+{
+    const keyCode = this.getAttribute('data-key');
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	audio.currentTime = 0;
+	audio.play();
+	const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    key.classList.add('playing');
 }
